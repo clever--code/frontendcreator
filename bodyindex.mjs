06deef0ffca15287ef8e154454e0f1958e4df846
Clever Code <clevercode.com.br@gmail.com>
2020-06-14 19:49:29 +0200

    Auto commit from FrontEnd Creator

--- bodyindex.mjs
+++ bodyindex.mjs
@@ -4,7 +4,9 @@ export default class BodyIndex extends HTMLElement {
         // attach a shadow allowing for accessibility from outside
         let shadow = this.attachShadow({ mode: 'open' });
         console.log('lad ad')
-        shadow.innerHTML = '<div>body aqui vai o bdy troquei a descrição ainda não entendo a mensagem</div>'
+        shadow.innerHTML = `
+        <a href='#a'>aqui tem um link  </a>
+        <div>body aqui vai o bdy troquei a descrição ainda não entendo a mensagem</div>`
 
 
     }
